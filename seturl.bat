f80d75fc4643f3533618411abd0701290647b0da
maxlicheng <40309661+maxlicheng@users.noreply.github.com>
2019-06-01 13:44:57 +0800

    Update seturl.bat
    创建开机自启动快捷方式脚本

--- seturl.bat
+++ seturl.bat
@@ -1,5 +1,4 @@
 
-::设置程序或文件的完整路径（必选）
 setlocal enableextensions
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.\
@@ -9,4 +8,3 @@ set Program=%DIRNAME%%LINK_NAME%
 set DEST_DIR=C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 
 copy /Y "%Program%" "%DEST_DIR%\%LINK_NAME%"
-cmd /k
